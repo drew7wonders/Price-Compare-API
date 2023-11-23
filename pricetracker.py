c5095ac52f93aee6523b5da6624cf9fb8ff8066f
Shubh Kesharwani <95687419+drew7wonders@users.noreply.github.com>
2023-11-23 23:06:09 +0530

    Update pricetracker.py

--- pricetracker.py
+++ pricetracker.py
@@ -33,6 +33,64 @@ def scrape_snapdeal(url):
 
     except Exception as e:
         return {"error": f"Error: {str(e)}"}
+    
+
+
+def scrape_flipkart(url):
+    try:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Accept': 'text/html'
+        }
+
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+
+            product_name = soup.find('span', class_='B_NuCI')
+            price_element = soup.find('div', class_='_30jeq3 _16Jk6d')
+
+            if price_element:
+                price = price_element.get_text(strip=True)
+                return {"product_name": product_name.text.strip(), "product_price": price}
+
+        return {"error": "Product is currently unavailable. Please try again later."}
+
+    except requests.exceptions.RequestException as e:
+        return {"error": f"Request Error: {str(e)}"}
+
+    except Exception as e:
+        return {"error": f"Error: {str(e)}"}
+    
+
+
+def scrape_amazon(url):
+    try:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Accept': 'text/html'
+        }
+
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+
+            product_name = soup.find('span', class_='a-size-large product-title-word-break')
+            price_element = soup.find('span', class_='a-price-whole')
+
+            if price_element:
+                price = price_element.get_text(strip=True)
+                return {"product_name": product_name.text.strip(), "product_price": price}
+
+        return {"error": "Product is currently unavailable. Please try again later."}
+
+    except requests.exceptions.RequestException as e:
+        return {"error": f"Request Error: {str(e)}"}
+
+    except Exception as e:
+        return {"error": f"Error: {str(e)}"}
 
 @app.route('/api/scrape_snapdeal', methods=['POST'])
 def handle_snapdeal_scraping():
@@ -41,7 +99,15 @@ def handle_snapdeal_scraping():
     url_snap = data.get('snapdeal_url', '')
     result_snap = scrape_snapdeal(url_snap)
 
-    return jsonify(result_snap)
+    
+    url_flip = data.get('flipkart_url', '')
+    result_flip = scrape_flipkart(url_flip)
+
+    url_amaz = data.get('amazon_url', '')
+    result_amaz = scrape_amazon(url_amaz)
+
+    ret = {"snap": result_snap,"flip": result_flip,"amaz": result_amaz}
+    return jsonify(ret)
 
 if __name__ == "__main__":
     # Use Gunicorn as the production server
